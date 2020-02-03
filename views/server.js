f99d46227d464489b08953791a40796cb386a5de
Khadijah Grimes <khadijahgrimes7@gmail.com>
2020-02-02 21:26:42 -0600

    updated files

--- views/server.js
+++ views/server.js
@@ -1,34 +1,39 @@
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var logger = require("morgan");
-
 var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+
+var PORT = process.env.PORT || 3000;
+
 var app = express();
 
-app.use(logger("dev"));
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+var router = express.Router();
 
-);
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(process.cwd() + "/public"));
-
-var exphbs = require("express-handelbars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebats", expressHandlebars({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/scraped_news");
-var db = mongoose.connection;
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-    console.log("Listening on PORT" + port);
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function (error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongooese connection is good");
+    }
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log("Listening on PORT" + port);
+app.listen(PORT, function () {
+    console.log("Listening on port:" + PORT);
 });
-
